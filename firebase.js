9a8b6ea70dbbf6305fc4a87048e0ee78909778fe
dewanagrofarm <erfandawyen@gmail.com>
2026-07-10 19:46:28 +0600

    Initialize Firebase app and database connection

--- firebase.js
+++ firebase.js
@@ -1,1 +1,19 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpAi3OjOg_obBpy5KC4tHuhG7xaPsR5xk",
+  authDomain: "dewan-family-heritage.firebaseapp.com",
+  databaseURL: "https://dewan-family-heritage-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "dewan-family-heritage",
+  storageBucket: "dewan-family-heritage.firebasestorage.app",
+  messagingSenderId: "39428944282",
+  appId: "1:39428944282:web:02382ebfafe47eba974a23",
+  measurementId: "G-0J1LZQ7JLH"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.db = db;
