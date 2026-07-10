8c0fea80d8271981cc5215d7fbbbd0d19d725cfc
dewanagrofarm <erfandawyen@gmail.com>
2026-07-10 21:25:38 +0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -13,3 +13,16 @@ window.saveData = function () {
 
   alert("Data Saved Successfully!");
 }
+import { ref, push } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+window.saveData = function () {
+  const name = document.getElementById("name").value;
+  const details = document.getElementById("details").value;
+
+  push(ref(window.db, "family"), {
+    name: name,
+    details: details
+  });
+
+  alert("ডাটা সেভ হয়েছে!");
+};
