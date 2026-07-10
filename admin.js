ff1ac7cb75089c88d2a41a3de8216327339590e0
dewanagrofarm <erfandawyen@gmail.com>
2026-07-10 20:10:03 +0600

    Add saveData function to push data to Firebase

--- admin.js
+++ admin.js
@@ -1,1 +1,15 @@
 
+import { db } from "./firebase.js";
+import { ref, push } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+window.saveData = function () {
+  const name = document.getElementById("name").value;
+  const details = document.getElementById("details").value;
+
+  push(ref(db, "family"), {
+    name: name,
+    details: details
+  });
+
+  alert("Data Saved Successfully!");
+}
